a2a982f2164d6e21ee82304b5a192592e7207e2d
Nicolas Hernandez Siachoque <n.hernandezs@uniandes.edu.co>
2018-05-01 18:03:50 -0500

    Anandido proyecto eclipse, el lenguaje reconoce crear la red P_RED a FIN_RED y en su contenido reconoce VAR y SITO

---
+++ ParserTesterClean/src/uniandes/teolen/parserJavaCC/mundoParser/MundoParsers.java
@@ -0,0 +1,168 @@
+package uniandes.teolen.parserJavaCC.mundoParser;
+
+import java.util.*;
+
+import Proyecto.Proyecto;
+import peliculas.peliculas;
+import uniandes.teolen.parserJavaCC.myParser.ParserCAML;
+import uniandes.teolen.parserJavaCC.myParserDR.ParserDR;
+import uniandes.teolen.parserJavaCC.newParser.NuevoParser;
+import uniandes.teolen.parserJavaCC.parserCAML.ParserCAMLco;
+import uniandes.teolen.parserJavaCC.parserProlog.ParserProlog;
+
+
+
+public class MundoParsers {
+	
+	// Nombres de los Parsers
+	private  ArrayList  <String> parsers  = new ArrayList <String> (); 
+	
+	// Parser que se est� usando
+	private int currentParser;
+	
+	
+	public  MundoParsers () {
+  	
+		// Agreguen al final de esta lista los nombres del nuevo parser
+		
+	    parsers.add("ParserDR");
+	    parsers.add("CAML -  interpreter");
+	    parsers.add("CAML");
+	    parsers.add("Nuevo Parser");
+	    parsers.add("Parser Prolog");
+	    parsers.add("Proyecto");
+	    
+	    currentParser =  0;
+
+	}
+	
+	public Proyecto getParserProyecto(){
+		return new Proyecto(System.in);
+	}
+	
+	public ParserProlog getParserProlog(){
+		return new ParserProlog(System.in);
+	}
+	
+	public ParserCAML getParserCAML(){
+		return new ParserCAML(System.in);
+	}
+	
+	public ParserCAMLco getParserCAMLco(){
+		return new ParserCAMLco(System.in);
+	}
+	
+	public ParserDR getParserDR(){
+		return new ParserDR();
+	}
+	
+	public NuevoParser getNuevoParser(){
+		return new NuevoParser(System.in);
+	}
+	
+	public String getStringCurrentParser(){
+		return parsers.get(currentParser);
+	}
+	
+	public int getCurrentParser() {
+		return currentParser;
+	}
+	
+	public void setCurrentParser(int p) {
+		currentParser = p;
+	}
+	
+	public String getParser(int i) {
+		return parsers.get(i);
+	}
+	
+	public int sizeParsers() {
+		return parsers.size();
+	}
+	
+	public String  procesarCadena(String texto) {
+		String resp = "";
+		
+		if(parsers.get(currentParser).equals("ParserDR")){
+			ParserDR parserDR = getParserDR();
+			parserDR.ReInit(new java.io.StringReader(texto));
+			try {
+		    	parserDR.Lexp(); 
+		    	resp = new String("OK    \n");
+		    }catch (Exception e) {
+		        resp = new String ("Error de Sintaxis: "+e.getMessage());
+		     } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		}
+		}
+		else if(parsers.get(currentParser).equals("CAML -  interpreter")){
+			ParserCAML parserCAML = getParserCAML();
+			parserCAML.ReInit(new java.io.StringReader(texto));
+			ArrayList <Integer>  myList = new ArrayList <Integer> ();
+			try {
+		    	myList = parserCAML.camlList(); 
+		    	resp = new String("OK    "+ myList + "\n");
+		    	 
+		    	
+			}catch (Exception e) {
+		        	resp = new String ("Error de Sintaxis: "+e.getMessage());
+		    } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		    }
+		}
+		else if(parsers.get(currentParser).equals("CAML")){
+			ParserCAML parserCAML = getParserCAML();
+			parserCAML.ReInit(new java.io.StringReader(texto));
+			ArrayList <Integer>  myList = new ArrayList <Integer> ();
+			try {
+		    	 parserCAML.camlList(); 
+		    	resp = new String("OK    \n");
+		    	 
+		    	
+			}catch (Exception e) {
+		        	resp = new String ("Error de Sintaxis: "+e.getMessage());
+		    } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		    }
+		}
+		else if(parsers.get(currentParser).equals("Nuevo Parser")){
+			NuevoParser nuevoParser = getNuevoParser();
+			nuevoParser.ReInit(new java.io.StringReader(texto));
+			try {
+		    	int v = nuevoParser.one_line(); 
+		    	resp = new String("OK   "+v+ "\n");
+		    }catch (Exception e) {
+		        resp = new String ("Error de Sintaxis: "+e.getMessage());
+		     } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		     }
+		}
+		else if(parsers.get(currentParser).equals("Parser Prolog")){
+			ParserProlog nuevoParser = getParserProlog();
+			nuevoParser.ReInit(new java.io.StringReader(texto));
+			try {
+		    	nuevoParser.hr(); 
+		    	resp = new String("OK    \n");
+		    }catch (Exception e) {
+		        resp = new String ("Error de Sintaxis: "+e.getMessage());
+		     } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		     }
+		}
+		else if(parsers.get(currentParser).equals("Proyecto")){
+			Proyecto nuevoParser = getParserProyecto();
+			Proyecto.ReInit(new java.io.StringReader(texto));
+			try {
+		    	Proyecto.one_line(); 
+		    	resp = new String("OK    \n");
+		    }catch (Exception e) {
+		        resp = new String ("Error de Sintaxis: "+e.getMessage());
+		     } catch (Error e) {
+		    	 resp = new String ("Error Lexico: "+e.getMessage());
+		     }
+		}
+
+		return "\n SISTEMA " + parsers.get(currentParser) + ": " + resp + "\n";
+	}
+
+}
